d0e4b5f8f983d5859945d4b045223ada410b45a3
Alisson Ally <alissonbrg32@gmail.com>
2024-09-03 13:41:06 -0300

    Requisição para Login em unica página

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LandingPage from "./components/LandingPage/LandingPage";
+import LandingPage from "./components/landingPage/LandingPage";
 import Maintenance from "./components/Maintenance";
-import Login from "./components/SignPages/Login";
-import Authenticate from "./components/SignPages/Authenticate";
-import Signup from "./components/SignPages/Signup";
-import CreateUser from "./components/SignPages/CreateUser";
-import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/signPages/Login";
+import Signup from "./components/signPages/Signup";
+import CreateUser from "./components/signPages/CreateUser";
+import Dashboard from "./components/dashboard/Dashboard";
 
 const routes = createBrowserRouter([
   {
@@ -19,10 +18,6 @@ const routes = createBrowserRouter([
   {
     path: "/loja",
     element: <Maintenance />,
-  },
-  {
-    path: "/authenticate",
-    element: <Authenticate />,
   },
   {
     path: "/create-user",
--- src/components/SignPages/Login.tsx
+++ src/components/SignPages/Login.tsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
 import "./AuthPage.css";
+import Input from "./Input";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function auth() {
+    axios
+      .post(
+        "https://crochesdatiabinha-backend-production.up.railway.app/api/auth/login",
+        {
+          username: username,
+          password: password,
+        }
+      )
+      .then((response) => {
+        if (response.data.token) {
+          //salva token
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        navigate("/login?err=" + err);
+        console.log({ err, password, username });
+      });
+  }
+
   return (
     <>
       <section className="auth-container flex-row">
@@ -8,54 +38,46 @@ function Login() {
           <div className="column image-column">
             <h1 className="image-side-title">Bem-vindo de volta!</h1>
           </div>
-          <div className="column">
-            <form action="/authenticate" method="get">
-              <h1>Faça Login</h1>
-              <p>Bem vindo devolta! Por favor, faça login na sua conta.</p>
-              <span className="input-label">Nome de Usuário</span>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                className="text-input"
-              />
-              <span className="input-label">Senha</span>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                className="text-input"
-              />
-              <div className="row space-between">
-                <div className="row">
-                  <label className="checkbox">
-                    <input
-                      type="checkbox"
-                      name="stay-logged"
-                      id="stay-logged"
-                      className="checkbox-input"
-                    />
-                    <span className="checkmark"></span>
-                  </label>
-                  <span className="input-label stay-logged">
-                    Lembrar de mim
-                  </span>
-                </div>
-                <span className="link">Esqueci minha senha</span>
+          <div className="column form">
+            <h1>Faça Login</h1>
+            <p>Bem vindo devolta! Por favor, faça login na sua conta.</p>
+            <Input
+              label="Nome de Usuário"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              label="Senha"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="row space-between">
+              <div className="row">
+                <label className="checkbox">
+                  <input
+                    type="checkbox"
+                    name="stay-logged"
+                    id="stay-logged"
+                    className="checkbox-input"
+                  />
+                  <span className="checkmark"></span>
+                </label>
+                <span className="input-label stay-logged">Lembrar de mim</span>
               </div>
-              <button className="auth-button" type="submit">
-                Entrar
-              </button>
-              <div className="sign-up-row row space-between">
-                <span className="sign-up">Ainda não tem uma conta? </span>
-                <span className="link">Registre-se</span>
-              </div>
-            </form>
+              <span className="link">Esqueci minha senha</span>
+            </div>
+            <button className="auth-button" onClick={auth}>
+              Entrar
+            </button>
+            <div className="sign-up-row row space-between">
+              <span className="sign-up">Ainda não tem uma conta? </span>
+              <span className="link">Registre-se</span>
+            </div>
           </div>
         </div>
       </section>
     </>
   );
 }
-
-export default Login;
