98d2977442ef14a07aedeb4d903daaa31bf4cad4
Alisson Ally <alissonbrg32@gmail.com>
2024-09-06 21:20:57 -0300

    Componente de contato criado e separado

--- src/components/pages/dashboardPage/components/painels/ContactsPainel.tsx
+++ src/components/pages/dashboardPage/components/painels/ContactsPainel.tsx
@@ -1,10 +1,18 @@
+import Contact from "../Contact";
+
 export default function Contacts() {
   return (
     <div>
       <div className="contacts-management">
         <div className="filter-row"></div>
         <div className="selected-contacts-row"></div>
-        <div className="contacts-list"></div>
+        <div className="contacts-list">
+          <Contact
+            name="Alisson Silva"
+            email="alissonbrg32@gmail.com"
+            message="Lorem ipsum aisjdaoisjdoaijsdojo"
+          />
+        </div>
       </div>
     </div>
   );
